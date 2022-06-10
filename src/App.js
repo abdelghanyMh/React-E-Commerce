41b533d268510e1a4c7b841f1d061ef00c683371
Mahammedi Abdelghani <wboductqr@relay.firefox.com>
2022-06-10 10:51:29 +0100

    configure: react routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 
+import {
+  BrowserRouter as Router, Routes, Route,
+} from "react-router-dom";
+
+// import pages 
+import {
+  Home,
+  About,
+  Products,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Error,
+  Private
+} from './pages'
+
+
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+
+      <Routes>
+        <Route path='/' element={<Home />}>
+          <Route path='about' element={<About />} />
+          <Route path='cart' element={<Cart />} />
+          <Route path='products' element={<Products />} >
+            <Route path=':id' element={<SingleProduct />} />
+          </Route>
+          <Route path='checkout' element={<Checkout />} />
+        </Route>
+        <Route path='*' element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router >
+  )
 }
 
 export default App
---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import  Home  from "./HomePage";
+import  About  from "./AboutPage";
+import  Products  from "./ProductsPage";
+import  SingleProduct  from "./SingleProductPage";
+import  Cart  from "./CartPage";
+import  Error  from "./ErrorPage";
+import  Checkout  from "./CheckoutPage";
+import  Private  from "./PrivateRoute";
+
+
+export {
+    Home,
+    About,
+    Products,
+    SingleProduct,
+    Cart,
+    Checkout,
+    Error,
+    Private
+}
