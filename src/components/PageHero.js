41c1a411443f683cfaf49f8f8e4ca9dc66cf4df9
Mahammedi Abdelghani <wboductqr@relay.firefox.com>
2022-06-13 15:43:40 +0100

    add: products page basic setup

--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-const PageHero = ({ title,product }) => {
+const PageHero = ({ title, product }) => {
+  console.log(product && '/');
   return <Wrapper>
     <div className="section-center">
       <h3>
-        <Link to="/">Home</Link>/
-        {product && '/' && <Link to='/products'>Products</Link>}
+        <Link to="/">Home</Link>
+        {product && <Link to='/products'>/ Products</Link>}
         / {title}
       </h3>
     </div>
