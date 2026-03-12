22dbc99600148d22755de9760215e5cb7044635c
Mahammedi Abdelghani <wboductqr@relay.firefox.com>
2026-03-12 08:30:38 +0100

    Create getProduct.js

---
+++ functions/getProduct.js
@@ -0,0 +1,23 @@
+const axios = require("axios");
+
+exports.handler = async function (event, context) {
+  const { id } = event.queryStringParameters;
+  try {
+    const response = await axios.get(
+      `https://course-api.com/react-store-single-product?id=${id}`
+    );
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(response.data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to fetch product" }),
+    };
+  }
+};
