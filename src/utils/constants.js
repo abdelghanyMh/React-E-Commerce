3d73bcc225ed920bb7056d966366e1cb68736729
Mahammedi Abdelghani <wboductqr@relay.firefox.com>
2026-03-12 08:31:10 +0100

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -42,6 +42,6 @@ export const services = [
   },
 ]
 
-export const products_url = 'https://course-api.com/react-store-products'
+export const products_url = '/.netlify/functions/getProducts'
 
-export const single_product_url = `https://course-api.com/react-store-single-product?id=`
+export const single_product_url = '/.netlify/functions/getProduct?id='
